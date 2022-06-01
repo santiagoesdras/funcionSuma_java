c1d99534439c6d2478f453f73f2056426c31f176
unknown <santiagoesdras501@gmail.com>
2022-06-01 01:32:02 -0600

    funcion de suma

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+ public class Main {
+
+
+    public static void main (String[] args){
+
+        System.out.println(suma(1,2,23));
+    }
+
+    public static float suma (float a, float b, float c) {
+
+        return a+b+c;
+    }
+}
+
